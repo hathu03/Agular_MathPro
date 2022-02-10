0cc7ed75c32060dfbf2e91a774a8f3ec7bade7ba
tiepcodegym <viettiep122@gmail.com>
2022-02-10 22:34:58 +0700

    bao_ve_route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
 import {NewfeedComponent} from "./newfeed/newfeed.component";
 import {NotFoundComponent} from "./not-found/not-found.component";
+import {AuthGuard} from "./auth.guard";
 
 const routes: Routes = [
   {
@@ -19,17 +20,20 @@ const routes: Routes = [
       {
         path: 'users',
         component: MasterComponent,
-        loadChildren: () => import('./components/users/users.module').then(m => m.UsersModule)
+        loadChildren: () => import('./components/users/users.module').then(m => m.UsersModule),
+        canActivate: [AuthGuard]
       },
       {
 
         path: 'posts',
         component: MasterComponent,
-        loadChildren: () => import('./components/posts/posts.module').then(m => m.PostsModule)
+        loadChildren: () => import('./components/posts/posts.module').then(m => m.PostsModule),
+        canActivate: [AuthGuard]
       },
       {
         path: 'newfeed',
-        component: NewfeedComponent
+        component: NewfeedComponent,
+        canActivate: [AuthGuard]
       },
       {
         path: 'master',
